da91b897b5567e2638b4ef2aee9f8ea59bd7f605
John Wana <john@wana.us>
2021-02-26 13:28:22 -0800

    fixup paths in final JS/TS files

--- build.sh
+++ build.sh
@@ -17,5 +17,13 @@ mv package.json package.bg.json
 jq 'del(.files,.types,.main,.module) + {"module": "index.js", "types": "index.d.ts"}' package.bg.json > package.json
 rm package.bg.json
 
+# fix all paths
+sed -i'.bak' 's/\.\.\/pkg\//\.\//g' *.ts
+sed -i'.bak' 's/\.\.\/pkg\//\.\//g' *.js
+rm *.bak
+
+# we're uploading all files
+rm .gitignore
+
 # done!
 cd ..
